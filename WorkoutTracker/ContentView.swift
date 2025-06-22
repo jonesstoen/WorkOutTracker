3c484341db83b0664d5574a4cc278fb9c28ca1b4
jonesstoen <jonesstoen@gmail.com>
2025-06-22 20:29:41 +0200

    Initial Commit

---
+++ WorkoutTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WorkoutTracker
+//
+//  Created by Johannes Støen on 22/06/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WorkoutTracker/WorkoutTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutTrackerApp.swift
+//  WorkoutTracker
+//
+//  Created by Johannes Støen on 22/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
